b48132e9de7879d6230efca283ad8cd8f0358a7d
Viktoriya Zavalishka <viza901@gmail.com>
2021-05-20 10:36:45 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { HelloWorldTemplateComponent } from './hello-world-template.component';
+import { HelloWorldNgIfComponent } from './hello-world-ngif/hello-world-ngif.component';
+import { HelloWorldDependencyInjectionComponent } from './hello-world-di/hello-world-di.component';
+import { HelloWorldInterpolationComponent } from './hello-world-interpolation/hello-world-interpolation.component';
+import { HelloWorldBindingsComponent } from './hello-world-bindings/hello-world-bindings.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HelloWorldComponent,
+    HelloWorldTemplateComponent,
+    HelloWorldNgIfComponent,
+    HelloWorldDependencyInjectionComponent,
+    HelloWorldInterpolationComponent,
+    HelloWorldBindingsComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
